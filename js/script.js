0058b5ecab253627c758bdd99627fb0f2bf10b06
Destonin1 <destonin@gmail.com>
2021-06-07 13:41:37 +0300

    12

--- js/script.js
+++ js/script.js
@@ -50,4 +50,35 @@ $(document).ready(function(){
  	$('li#li3').css({"border": "2px solid transparent", "border-bottom": "3px solid transparent"});
  });
 
+  $('.slider_top').slick({
+        autoplay: true,
+          autoplaySpeed: 5000,
+        prevArrow: '<button type="button" class="ss-btn slide-slick-next"><img src="img/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="ss-btn slide-slick-prev"><img src="img/arrow-right.png"></button>',
+        responsive: [
+        {
+          breakpoint: 576,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+        }
+        ] 
+      });
+
+      $('.slider-middle-items').slick({
+        arrows: false,
+        dots: true,
+        customPaging : function(slider) {
+              var thumb = $(slider.slides).data('thumb');
+            return '<div class="dot-play"><img src="img/icon1.png"></div><p class="under_text">Responsive Websites</p>';
+            }
+      });
+
+      $('.fa-bars').on('click', function() {
+        $('.menu_small').toggleClass('menu_small_active');
+      });
+
+      $('.footer_slider').slick();
+
 });
